d135456ebaa3a5dfb65150765d0bf9d163331738
sensitiky <tikybenitez@gmail.com>
2024-06-25 11:14:14 -0300

    AYUDA

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
-import withNextIntl from 'next-intl/plugin';
-
-const isProduction = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['sensitiky.github.io'],
-  },
+    reactStrictMode: true,
+    images: {
+      domains: ['sensitiky.github.io'],
+    },
 };
 
-const finalConfig = withNextIntl()(nextConfig);
-
-export default isProduction ? finalConfig : nextConfig;
+export default nextConfig;
