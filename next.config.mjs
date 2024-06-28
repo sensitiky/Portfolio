44fcf5acb1122a1e73107be00baffbcf0726143e
sensitiky <tikybenitez@gmail.com>
2024-06-28 18:50:31 -0300

    Final

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    images: {
-      domains: ['sensitiky.github.io'],
-    },
 };
 
 export default nextConfig;
