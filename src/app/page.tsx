0bc3a7ff313d5a0d24fda062a69c82429e60a434
sensitiky <tikybenitez@gmail.com>
2024-06-30 23:49:37 -0300

    Resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -142,7 +142,6 @@ const Main: React.FC = () => {
                 >
                   Contact
                 </Link>
-                <Link href="/formulario"></Link>
               </div>
             </SheetContent>
           </Sheet>
@@ -162,7 +161,7 @@ const Main: React.FC = () => {
               </p>
               <div className="flex gap-4">
                 <Link
-                  href="/CV-Correa Mario .pdf"
+                  href="/CV-Correa Mario.pdf"
                   className="inline-flex items-center justify-center rounded-md bg-primary px-6 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
                   prefetch={false}
                 >
